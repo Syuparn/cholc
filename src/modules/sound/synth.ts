0a1ad3bb43a06878b99f0bf23d3fb345f879d91c
syuparn <s.hello.spagetti@gmail.com>
2025-01-04 15:46:35 +0900

    change volume

--- src/modules/sound/synth.ts
+++ src/modules/sound/synth.ts
@@ -1,6 +1,8 @@
 import * as Tone from "tone";
 
-const synth = new Tone.PolySynth().toDestination();
+const synth = new Tone.PolySynth({volume: -14}).toDestination();
+
+// TODO: add setter for synth.volume.value
 
 export function playChord(notes: string[]): void {
   synth.triggerAttackRelease(notes, "4n");
